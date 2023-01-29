6bf2be06d8434e030e92e2b1b672ce3cab5ab0b1
ThalmisFaraj <106799941+ThalmisFaraj@users.noreply.github.com>
2023-01-29 10:50:26 +0530

    add comments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+// import ReactDom from "react-dom";
 import "./Sidebar.css";
 // ====Components ===========
 import SidebarOptions from "./SidebarOptions";
